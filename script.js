7d6d00d9d61e609d925674ae8aa0209a57251f1f
Jorge Abdallah <jorgeea1205@gmail.com>
2020-08-26 23:43:07 -0400

    Wrote the base code for a traditional rock-paper-scissors game to be played on the console. Next step is working on the UI

---
+++ script.js
@@ -0,0 +1,68 @@
+let rock = {
+    identity: 'ROCK',
+    weakness: 'PAPER',
+    goodAgnst: 'SCISSORS'
+};
+let scissors = {
+    identity: 'SCISSORS',
+    weakness: 'ROCK',
+    goodAgnst: 'PAPER'
+};
+let paper = {
+    identity: 'PAPER',
+    weakness: 'SCISSORS',
+    goodAgnst: 'ROCK'
+};//The three options and their properties
+let options = [rock,paper,scissors];//For the computer to choose one
+
+let question;
+let player;
+let result;
+let compChoice;
+
+function compChoose() {
+    compChoice = options[random(3)];
+}
+
+function random(n) { //Random integer generator
+    return Math.floor(Math.random() * n);
+}
+function preparation(){
+    question = prompt('Rock, paper or scissors?');
+}
+
+function playerChoosing(){
+    if (question.toUpperCase() == 'ROCK'){
+        player = rock;
+    }else if (question.toUpperCase() == 'PAPER'){
+        player = paper;
+    }else if (question.toUpperCase() == 'SCISSORS'){
+        player = scissors;
+    }else {
+        alert('Please choose a valid option');
+        preparation();
+    }
+}
+
+function fight(player,computer){
+    if (player.identity === computer.identity){
+        console.log('Its a tie!');
+    }else if (player.weakness === computer.identity) {
+        console.log('You lose!');
+    } else if (player.goodAgnst === computer.identity) {
+        console.log('You win!');
+    }
+}
+
+function duel(){
+    preparation();
+    playerChoosing();
+    
+    if (player)  {
+    compChoose();
+    console.log(`You chose ${player.identity}`);
+    console.log(`Computer chose ${compChoice.identity}`);
+    fight(player,compChoice);
+}
+}
+
